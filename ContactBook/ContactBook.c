925691f0e0cc3e02d83dea47abc3932cac608e0b
Krzysztof Płazio <krzpla.389@edu.erzeszow.pl>
2021-12-13 22:09:04 +0100

    jakies poczatki

--- ContactBook/ContactBook.c
+++ ContactBook/ContactBook.c
@@ -1,16 +1,22 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+#include <mysql/mysql.h>
+bool goodLogin()
+{
+	MYSQL mysql;
+	return false;
+}
 
 void logging()
 {
 	char login[32], pass[32];
 	printf("Your personal phone book!\n");
 	printf("Login: ");
-	scanf_s("%s", &login);
+	scanf("%s", &login);
 	printf("Password: ");
-	scanf_s("%s", &pass);
+	scanf("%s", &pass);
 	printf("Checking input...\n");
-	// look for good password
+	
 	printf("Logging succesfull!\n");
 	system("cls");
 }
