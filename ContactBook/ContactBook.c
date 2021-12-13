bb240fb254a1262ce39b7e428517badc1dd9d990
krzysztof <krzysztof@KRZYSZTOF>
2021-12-13 22:05:10 +0100

    Dodaj pliki projektów.

---
+++ ContactBook/ContactBook.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void logging()
+{
+	char login[32], pass[32];
+	printf("Your personal phone book!\n");
+	printf("Login: ");
+	scanf_s("%s", &login);
+	printf("Password: ");
+	scanf_s("%s", &pass);
+	printf("Checking input...\n");
+	// look for good password
+	printf("Logging succesfull!\n");
+	system("cls");
+}
+
+int main(void)
+{
+	logging();
+}
