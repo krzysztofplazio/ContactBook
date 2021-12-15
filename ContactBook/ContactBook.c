d85ca5d841b7c17fe994a7d8d8ccf25e5a22fc72
Krzysztof Płazio <krzpla.389@edu.erzeszow.pl>
2021-12-15 19:48:57 +0100

    XDXD

--- ContactBook/ContactBook.c
+++ ContactBook/ContactBook.c
@@ -12,40 +12,98 @@ char* server = "localhost";
 char* user = login;
 char* password = pass;
 char* database = "bazaphp1";
- 
+
+void mainMenu();
+
+void backToMenu()
+{
+	printf("0. Back to main menu");
+}
+
+void getContactById()
+{
+	system("cls");
+}
+
+void searchContacts()
+{
+	system("cls");
+}
+
 void showAllContacts()
 {
 	system("cls");
 	MYSQL_ROW rows;
 	char query[] = "SELECT Id, Name, SName, PhoneNumber, Address, Email FROM contacts_";
+	int lastId = 0;
 	strcat(query, login);
+	bool isExists = false;
 	if (mysql_query(conn, query))
 	{
 		fprintf(stderr, "%s\n", mysql_error(conn));
 		exit(1);
 	}
-
+	printf("Here's yours all contacts:\n");
 	MYSQL_RES* response = mysql_use_result(conn);
 	int num_fields = mysql_num_fields(response);
 	while ((rows = mysql_fetch_row(response)) != NULL)
 	{
 		if (num_fields > 0)
 		{
+			lastId = rows[0];
 			printf("%s. Name: %s Surname: %s PhoneNumber: %s Address: %s \n", rows[0], rows[1], rows[2], rows[3], rows[4]);
+			isExists = true;
 		}
 		else
 		{
 			printf("You haven't any contacts.");
+			isExists = false;
 		}
 	}
 	mysql_free_result(response);
+	if (isExists)
+	{
+		char quit[1];
+		printf("Exit? [y/N]: ");
+		scanf("%s", &quit[0]);
+
+		putchar('\n');
+		switch (quit[0])
+		{
+		case 121:
+		{
+			mainMenu();
+		}
+		break;
+		case 78:
+		{
+			int conNum;
+			printf("Pick your contact number: ");
+			scanf("%d", &conNum);
+			if (conNum > lastId || lastId == 0)
+			{
+				printf("There's not contact with id %d", conNum);
+			}
+			else
+			{
+				getContactById(conNum);
+			}
+		}
+		break;
+		default:
+		{
+			printf("Pick a correct one: ");
+		}
+		break;
+		}
+	}
 }
 
 void mainMenu()
 {
 	system("cls");
 	int choose;
-	printf("Welcom in your personal Contact Book %s!\nWhat do you want to do now?\n", login);
+	printf("Welcome in your personal Contact Book %s!\nWhat do you want to do now?\n", login);
 	printf("1. Show my all contacts.\n");
 	printf("2. Search contact.\n");
 	printf("3. Add new contact.\n");
@@ -57,6 +115,11 @@ void mainMenu()
 	case 1:
 	{
 		showAllContacts();
+	}
+	break;
+	case 2:
+	{
+		searchContacts();
 	}
 	break;
 	default:
